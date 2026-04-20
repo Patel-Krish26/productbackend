c37c47bd4a1623014a8db97e2fee3eaf30619b05
Patel-Krish26 <krishpr2004@gmail.com>
2026-04-20 06:24:38 +0000

    backend setup completed

--- src/main/java/com/example/productbackend/ProductbackendApplication.java
+++ src/main/java/com/example/productbackend/ProductbackendApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProductbackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProductbackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ProductbackendApplication.class, args);
+    }
 }
